07bf54fad49657e18b0a369e69e0f00baee01fdf
Alatyami <alatyami@gmail.com>
2023-05-20 23:00:09 -0600

    Progress #3 Implemented Mixing Vat Recipes

--- src/main/java/growthcraft/milk/recipe/CheesePressRecipe.java
+++ src/main/java/growthcraft/milk/recipe/CheesePressRecipe.java
@@ -1,4 +1,49 @@
 package growthcraft.milk.recipe;
 
-public class CheesePressRecipe {
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+
+public class CheesePressRecipe implements Recipe<SimpleContainer> {
+
+
+    @Override
+    public boolean matches(SimpleContainer p_44002_, Level p_44003_) {
+        return false;
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer p_44001_, RegistryAccess p_267165_) {
+        return null;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int p_43999_, int p_44000_) {
+        return false;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess p_267052_) {
+        return null;
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return null;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return null;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return null;
+    }
 }
