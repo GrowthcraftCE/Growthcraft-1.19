621db6b640fbd86b6998f21b717f690473887c8d
Alatyami <alatyami@gmail.com>
2023-05-21 20:27:55 -0600

    Started #12 Cheese Wheels

--- src/main/java/growthcraft/milk/block/CheeseWheelBlock.java
+++ src/main/java/growthcraft/milk/block/CheeseWheelBlock.java
@@ -1,5 +1,56 @@
 package growthcraft.milk.block;
 
-public class CheeseWheelBlock {
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+public class CheeseWheelBlock extends HorizontalDirectionalBlock {
     //TODO[12]: Implement CheeseWheelBlock
+    public static final BooleanProperty AGED = BooleanProperty.create("aged");
+
+    public static final IntegerProperty SLICE_COUNT_TOP = IntegerProperty.create("slicestop", 0, 4);
+    public static final IntegerProperty SLICE_COUNT_BOTTOM = IntegerProperty.create("slicesbottom", 0, 4);
+
+    public static final VoxelShape BOUNDING_BOX = Block.box(
+            1.00F, 0.0F, 1.0F,
+            15.0F, 16.0F, 15.0F
+    );
+
+    public static final VoxelShape BOUNDING_BOX_HALF = Block.box(
+            1.00F, 0.0F, 1.0F,
+            15.0F, 8.0F, 15.0F
+    );
+
+    private final int color;
+
+    public CheeseWheelBlock(Color color) {
+        super(getInitProperties());
+        this.color = color.getRGB();
+    }
+
+    public static Properties getInitProperties() {
+        Properties properties = Properties.copy(Blocks.CAKE);
+        properties.noOcclusion();
+        return properties;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        super.createBlockStateDefinition(stateBuilder.add(FACING, AGED, SLICE_COUNT_BOTTOM, SLICE_COUNT_TOP));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(SLICE_COUNT_BOTTOM, 4).setValue(SLICE_COUNT_TOP, 0).setValue(AGED, false);
+    }
 }
