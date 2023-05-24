79516910c6ce730d98617cd3136905e4964b33f7
Alatyami <alatyami@gmail.com>
2023-05-23 20:38:15 -0600

    Progress #63 Implemented CheesePressBlock interaction

--- src/main/java/growthcraft/milk/block/CheesePressBlock.java
+++ src/main/java/growthcraft/milk/block/CheesePressBlock.java
@@ -5,16 +5,13 @@ import growthcraft.milk.GrowthcraftMilk;
 import growthcraft.milk.block.entity.CheesePressBlockEntity;
 import growthcraft.milk.init.GrowthcraftMilkBlockEntities;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -39,7 +36,10 @@ public class CheesePressBlock extends BaseEntityBlock {
 
     public static final IntegerProperty ROTATION = IntegerProperty.create("rotation", 0, 7);
 
-    public static final VoxelShape BOUNDING_BOX = Block.box(0.01F, 0.0F, 0.01F, 15.98F, 15.98F, 15.98);
+    public static final VoxelShape BOUNDING_BOX = Block.box(
+            0.01F, 0.0F, 0.01F,
+            15.98F, 15.98F, 15.98
+    );
 
     public CheesePressBlock() {
         this(getInitProperties());
@@ -77,7 +77,7 @@ public class CheesePressBlock extends BaseEntityBlock {
     }
 
     @Override
-    public PushReaction getPistonPushReaction(BlockState blockState) {
+    public PushReaction getPistonPushReaction(BlockState p_60584_) {
         return PushReaction.DESTROY;
     }
 
@@ -103,13 +103,13 @@ public class CheesePressBlock extends BaseEntityBlock {
 
     @Override
     public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
-
-        if (level != null && !level.isClientSide) {
+        // If player has the wrench, then tighten or loosen the ROTATION. Crouching tightens the vice, otherwise loosen it.
+        if(level != null && !level.isClientSide) {
             CheesePressBlockEntity blockEntity = (CheesePressBlockEntity) level.getBlockEntity(blockPos);
-            if (blockEntity == null) return InteractionResult.FAIL;
+            if(blockEntity == null) return InteractionResult.FAIL;
 
-            if (player.getItemInHand(interactionHand).getItem() == GrowthcraftItems.WRENCH.get()) {
-                if (!player.isCrouching()) {
+            if(player.getItemInHand(interactionHand).getItem() == GrowthcraftItems.WRENCH.get()) {
+                if(!player.isCrouching()) {
                     // Then tighten the cheese press.
                     level.playSound(null, blockPos, SoundEvents.CHAIN_PLACE, SoundSource.BLOCKS);
                     level.setBlock(blockPos, blockState.setValue(ROTATION, blockEntity.doRotation(true)), Block.UPDATE_ALL_IMMEDIATE);
@@ -118,24 +118,19 @@ public class CheesePressBlock extends BaseEntityBlock {
                     level.playSound(null, blockPos, SoundEvents.CHAIN_BREAK, SoundSource.BLOCKS);
                     level.setBlock(blockPos, blockState.setValue(ROTATION, blockEntity.doRotation(false)), Block.UPDATE_ALL_IMMEDIATE);
 
-                    // Retrieve the itemstack in the output slot.
                     ItemStack resultItemStack = blockEntity.getItemStackHandler().getStackInSlot(1).copy();
-                    if (!resultItemStack.isEmpty() && !player.getInventory().add(resultItemStack)) {
+                    if(!resultItemStack.isEmpty() && !player.getInventory().add(resultItemStack)) {
                         ItemEntity itemEntity = new ItemEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), resultItemStack);
                         level.addFreshEntity(itemEntity);
                     }
-
-                    // Set the output slot to air.
-                    blockEntity.getItemStackHandler().setStackInSlot(1, new ItemStack(Items.AIR));
                 }
 
                 return InteractionResult.SUCCESS;
-            } else if (!player.getItemInHand(interactionHand).isEmpty() && blockEntity.isOpen() && blockEntity.getItemStackHandler().getStackInSlot(0).isEmpty()) {
-                // Insert one count of the item being held.
+            } else if (!player.getItemInHand(interactionHand).isEmpty() && blockEntity.isOpen()
+                        && blockEntity.getItemStackHandler().getStackInSlot(0).isEmpty()){
                 ItemStack itemStackToInsert = player.getItemInHand(interactionHand).copy();
                 itemStackToInsert.setCount(1);
                 blockEntity.getItemStackHandler().insertItem(0, itemStackToInsert, false);
-
                 player.getItemInHand(interactionHand).shrink(1);
 
                 return InteractionResult.SUCCESS;
@@ -157,22 +152,5 @@ public class CheesePressBlock extends BaseEntityBlock {
             }
         }
         super.onRemove(blockState, level, blockPos, newBlockState, isMoving);
-    }
-
-    public static void makeParticles(Level level, BlockPos blockPos, BlockState blockState) {
-        RandomSource randomSource = level.getRandom();
-
-        CheesePressBlockEntity blockEntity = (CheesePressBlockEntity) level.getBlockEntity(blockPos);
-
-        if (blockState.getValue(ROTATION) == 7 && blockEntity.getTickClock("current") > 0) {
-            if (randomSource.nextInt(16) <= 4) {
-                double d0 = (double) blockPos.getX() + randomSource.nextDouble() / 2.0D;
-                double d1 = (double) blockPos.getY() - 0.05D;
-                double d2 = (double) blockPos.getZ() + randomSource.nextDouble() / 2.0D;
-
-                level.addParticle(ParticleTypes.FALLING_HONEY, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-            }
-        }
-
     }
 }
